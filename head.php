aff40baef9e2eb86520c5f47fbd4d7f54524e98f
RafaelMarangoni <rafael.dmarangoni89@gmail.com>
2019-07-01 09:16:56 -0300

    realizaçao da pagina principal. criaçao das tabelas, necessariamente agora so falta a parte logica

--- head.php
+++ head.php
@@ -20,7 +20,7 @@
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="projetoIntegrador.php">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="projetoIntegrador.php">Home<span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="about.php">About</a>
--- projetoIntegrador.php
+++ projetoIntegrador.php
@@ -1,12 +1,16 @@
 <?php
 
 $produtos = [
-  ["camiseta"=>"Camisa A","descricao"=>"loren","preco"=>99,99],
-  ["camiseta"=>"camisa B","descricao"=>"loren","preco"=>99,99],
-  ["camiseta"=>"camiseta C","descricao"=>"loreeeenn","preco"=>99,99]
+  ["camiseta"=>"Camisa A","descricao"=>"loren","preco"=>99,99,"img"=>"imagens/produto1.jpeg"],
+  ["camiseta"=>"camisa B","descricao"=>"loren","preco"=>99,99,"img"=>"imagens/produto2.jpg"],
+  ["camiseta"=>"camiseta C","descricao"=>"loreeeenn","preco"=>99,99,"img"=>"imagens/produto3.jpg"]
 
 ];
-
+$carrossel = [
+  ["img2"=>"imagens/bannerprincipal1.jpg"],
+  ["img2"=>"imagens/bannerprincipal2.jpg"],
+  ["img2"=>"imagens/banner11.jpg"]
+];
 
 ?>
 <!DOCTYPE html>
@@ -17,59 +21,60 @@ $produtos = [
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Projeto Integrador</title>
+  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 
 <body>
-<?= require_once "head.php" ?>
+  <?= require_once "head.php" ?>
 
-<section class= "container">
-  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="imagens/carrossel1.jpg" class="d-block w-100" alt="...">
+  <section class="container-fluid col-lg-12 px-lg-0 " class="carrossel" style="">
+    <div id="carouselExampleFade" class="carousel slide carousel-fade imgCarr" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="imagens/bannerprincipal1.jpg" class="d-block w-100 imgCarr" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="imagens/bannerprincipal2.jpg" class="d-block w-100 imgCarr" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="imagens/banner11.png" class="d-block w-100 imgCarr" alt="...">
+        </div>
       </div>
-      <div class="carousel-item">
-        <img src="imagens/carrossel3.jpg" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="imagens/carrossel12.jpg" class="d-block w-100" alt="...">
-      </div>
+      <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div><br>
   </section>
-  
-  <section class="container ">
-  <?php foreach($produtos as $produto){?>
-    <div class="container">
-  
-      <div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img src="Imagens/carrossel1.jpg" class="card-img-top" alt="...">
+
+  <section class="container">
+  <div class="row">
+      <div class="col-lg-12 d-flex justify-content-between text-center">
+        <?php foreach($produtos as $produto){?>
+        <div class="card customCard" style="width:18rem;">
+          <img src="<?= $produto['img']?>" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title"><?= $produto['camiseta']?></h5>
             <p class="card-text"><?= $produto['descricao'] ?></p>
             <a href="#" class="btn btn-primary"><?= $produto['preco']?></a>
           </div>
         </div>
-      
+        <?}?>
       </div>
-     
-
+    </div>
   </section>
-  <?}?>
+
   <br>
- 
+
   <!-- Footer -->
+
   <?= require_once "footer.php"?>
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
